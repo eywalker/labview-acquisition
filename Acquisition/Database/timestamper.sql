320c06f4c2c4bf75da848b36bed017d87ce17c10
rcotton <rcotton@d1c95485-6871-e240-9df0-63fa95a8ea69>
2011-06-07 17:46:36 +0000

    Adding stop times to database
    git-svn-id: svn://at-lab/Labview@3762 d1c95485-6871-e240-9df0-63fa95a8ea69

--- Acquisition/Database/timestamper.sql
+++ Acquisition/Database/timestamper.sql
@@ -27,6 +27,7 @@ CREATE TABLE ephys (
 	setup TINYINT UNSIGNED NOT NULL,
 	session_start_time DOUBLE NOT NULL,
 	ephys_start_time DOUBLE NOT NULL,
+	ephys_stop_time DOUBLE,
 	ephys_path LONGTEXT NOT NULL,
 	FOREIGN KEY (setup, session_start_time) REFERENCES sessions(setup,session_start_time) ON DELETE RESTRICT ON UPDATE RESTRICT,
 	PRIMARY KEY (setup, session_start_time, ephys_start_time)
@@ -37,6 +38,7 @@ CREATE TABLE stimulation (
 	setup TINYINT UNSIGNED NOT NULL,
 	session_start_time DOUBLE NOT NULL,
 	stim_start_time DOUBLE NOT NULL,
+	stim_stop_time DOUBLE,
 	stim_path LONGTEXT NOT NULL,
 	exp_type VARCHAR(45) NOT NULL,
 	total_trials INT,
@@ -62,6 +64,7 @@ CREATE TABLE behavior_traces (
 	session_start_time DOUBLE NOT NULL,
 	stim_start_time DOUBLE NOT NULL,
 	beh_start_time DOUBLE NOT NULL,
+	beh_stop_time DOUBLE,
 	beh_path LONGTEXT NOT NULL,
 	beh_traces_type VARCHAR(45) NOT NULL,
 	FOREIGN KEY (setup, session_start_time, stim_start_time) REFERENCES stimulation(setup,session_start_time,stim_start_time) ON DELETE RESTRICT ON UPDATE RESTRICT,
