ae1250ddd575943f01a453fa30f7385b8aee1e66
Saumil S Patel <spatel@cns.bcm.edu>
2013-11-05 13:16:32 -0600

    updated the test program

--- Acquisition/AOD_Control/MirrorAndAodMP/testclientserver3dinterp.m
+++ Acquisition/AOD_Control/MirrorAndAodMP/testclientserver3dinterp.m
@@ -7,7 +7,7 @@ data = rand(400,400,100) ;
 
 localoutdata = resample3Dimage(data,1,1,2) ;
 
-outdata = client3DInterp1(single(data),1,1,2) ;
+outdata = client3DInterp1(single(data),1,1,2, '128.249.80.146', 'v:\temp') ;
 
 diffdata = localoutdata-outdata ;
 max(diffdata(:))
